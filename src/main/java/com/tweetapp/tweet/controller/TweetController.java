048c9c1b367a2d7a6eca852ebffa80c03d6ac018
Akshya K <akshayakumar1811@gmail.com>
2022-09-28 00:07:44 +0530

    Second commit

--- src/main/java/com/tweetapp/tweet/controller/TweetController.java
+++ src/main/java/com/tweetapp/tweet/controller/TweetController.java
@@ -31,7 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.Generated;
 
 @RequestMapping(value = "/api/v1.0/tweets")
-@CrossOrigin("https://fsetweetappfrontend.azurewebsites.net")
+@CrossOrigin("http://localhost:4200")
 @RestController
 @Generated
 public class TweetController {
